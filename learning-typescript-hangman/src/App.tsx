6f6c174ed3633cc51b4e52f3b06d23f2e7cf969c
woojin <woojin.seo@icloud.com>
2024-05-22 16:20:21 -0500

    Day 3 Hangman p1

---
+++ learning-typescript-hangman/src/App.tsx
@@ -0,0 +1,41 @@
+import {useState} from "react"
+import words from "./wordList.json"
+import { HangManWord } from "./HangManWord"
+import { HangManDrawing } from "./HangManDrawing"
+import { Keyboard } from "./Keyboard"
+
+function App () {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  }
+)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+    )
+
+  return <div style = {{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center"
+  }}>
+    <div style= {{
+      fontSize: "2rem",
+      textAlign: "center"
+    }}>
+      Lose
+      Win
+    </div>
+    <HangManDrawing numberOfGuesses = {incorrectLetters.length}/>
+    <HangManWord />
+    <div style = {{ alignSelf: "stretch"}} >
+      <Keyboard />
+    </div>
+  </div>
+}
+
+export default App
