96ff2e3a2ddf3db25e11b284b4ecebe1d4477469
woojin <woojin.seo@icloud.com>
2024-06-03 16:03:14 -0500

    Day 11 Shopping Cart

--- learning-typescript-shopping-cart/src/context/ShoppingCartContext.tsx
+++ learning-typescript-shopping-cart/src/context/ShoppingCartContext.tsx
@@ -4,6 +4,11 @@ type ShoppingCartProviderProps = {
   children: ReactNode
 }
 
+type CartItem = {
+  id: number;
+  quantity: number
+}
+
 type ShoppingCartContext = {
   getItemQuantity: (id: number) => number
   increaseCartQuantity: (id: number) => void
@@ -19,7 +24,53 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider( {children}: ShoppingCartProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
-  return<ShoppingCartContext.Provider value = {{}}>
+
+  const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
+
+  function getItemQuantity(id: number) {
+    return cartItems.find(item => item.id === id)?.quantity || 0
+  }
+
+  function increaseCartQuantity(id:number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id) == null) {
+        return [...currItems, {id, quantity: 1}]
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity + 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+
+  function decreaseCartQuantity(id:number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id)?.quantity === 1) {
+        return currItems.filter(item => item.id !== id)
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity - 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  function removeFromCart(id:number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id)
+    })
+  }
+  
+  return<ShoppingCartContext.Provider value = {{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartQuantity}}>
     {children}
   </ShoppingCartContext.Provider>
 }
