095c970bdc6c64a6282d3f647c706c5eead9d2f0
woojin <woojin.seo@icloud.com>
2024-06-26 16:04:01 -0500

    Tanstack table completed

--- learning-table/src/App.tsx
+++ learning-table/src/App.tsx
@@ -1,11 +1,74 @@
 import './App.css'
+import { DateTime } from 'luxon'
+import { useMemo } from 'react'
 import BasicTable from './components/BasicTable'
+import movies from './MOVIE_DATA.json'
 
 function App() {
+  const data = useMemo(() => movies, [])
+  /** @type import('@tanstack/react-table').ColumnDef<any> */
+  const columns = [
+    {
+      header: 'ID',
+      accessorKey: 'id',
+      footer: 'ID',
+    },
+    {
+      header: 'Name',
+      columns: [
+        {
+          header: 'First',
+          accessorKey: 'first_name',
+          footer: 'First_Name',
+        },
+        {
+          header: 'Last',
+          accessorKey: 'last_name',
+          footer: 'Last_Name',
+        },
+      ],
+    },
+    {
+      header: 'Email',
+      accessorKey: 'email',
+      footer: 'Email',
+    },
+    {
+      header: 'Gender',
+      accessorKey: 'gender',
+      footer: 'Gender',
+    },
+    {
+      header: 'Date of birth',
+      accessorKey: 'dob',
+      footer: 'Date of birth',
+      cell: info =>
+        DateTime.fromISO(info.getValue()).toLocaleString(DateTime.DATE_MED),
+    },
+  ]
+
+  const movieColumns = [
+    {
+      header: 'ID',
+      accessorKey: 'id',
+    },
+    {
+      header: 'Name',
+      accessorKey: 'name',
+    },
+    {
+      header: 'Genre',
+      accessorKey: 'genre',
+    },
+    {
+      header: 'Rating',
+      accessorKey: 'rating',
+    },
+  ]
   return (
     <>
     <h1>React-table</h1>
-    <BasicTable/>
+    <BasicTable data={data} columns = {movieColumns}/>
     </>
   )
 }
